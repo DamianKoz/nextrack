c4610c87bb61b35a9f26a6a8ae694dc61f58757f
Julian Fritz <programmming.jf@gmail.com>
2023-10-10 21:55:54 +0200

    feat: add code for connecting to postgres db

--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+
+	_ "github.com/jackc/pgx/v5"
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 func main() {
 	port := ":8000"
+
+	_, err := connectToDB()
+	if err != nil {
+		log.Panic("Can't connect to Postgres.")
+	}
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/events", getAllEvents)
@@ -14,6 +24,22 @@ func main() {
 	http.HandleFunc("/events", getAllEvents)
 	fmt.Printf("Listening on Port %s\n", port)
 	http.ListenAndServe(port, nil)
+}
+
+func connectToDB() (*sql.DB, error) {
+	dsn := "postgres://ffkmhklc:QtN3se_l72ZTcDgi12K0V1WdY5MMS3i-@flora.db.elephantsql.com/ffkmhklc"
+	log.Printf("Connecting to dsn: %s", dsn)
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		log.Panicf("Encountered error while connecting to db: %s", err)
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Panicf("Encountered error while pinging to db: %s", err)
+		return nil, err
+	}
+	return db, err
 }
 
 func getAllEvents(w http.ResponseWriter, r *http.Request) {
