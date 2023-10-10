3e72da27bc200a1034adcab0968f8170057e9ae1
Damian <damian.makoza@gmail.com>
2023-10-10 19:52:08 +0200

    First commit

--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	port := ":8000"
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/events", getAllEvents)
+
+	http.HandleFunc("/events", getAllEvents)
+	fmt.Printf("Listening on Port %s\n", port)
+	http.ListenAndServe(port, nil)
+}
+
+func getAllEvents(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("alle Events"))
+}
